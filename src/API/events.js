b7ebafdf106949befb9c442853dccfd594a9fd38
Allybe <creeperplay1212@gmail.com>
2023-10-17 13:14:36 -0500

    Request added, and CLI args

---
+++ src/API/events.js
@@ -0,0 +1,18 @@
+// https://www.thebluealliance.com/apidocs/v3
+
+const axios = require("axios");
+const BASE_URL = "https://www.thebluealliance.com/api/v3";
+
+// GET /event/{event_key}/teams/simple
+async function eventTeamsSimple(token, eventKey) {
+    const url = BASE_URL + `/event/${eventKey}/teams/simple`;
+
+    try {
+        const request = await axios.get(url);
+        return request.data;
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+module.exports = { eventTeamsSimple };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-const TBA_KEY = "";
+const path = require("path");
+const fs = require("fs");
+const axios = require("axios");
+
+const events = require("./API/events");
+
+const TBA_TOKEN = "";
+const OUTPUT = path.join(__dirname + "/output");
+
+axios.defaults.headers.common["X-TBA-Auth-Key"] = TBA_TOKEN;
+
+const ARGS = process.argv;
+
+if (!fs.existsSync(OUTPUT)) {
+    fs.mkdirSync(OUTPUT);
+    console.log("Output folder created");
+}
+
+(async function () {
+    const teams = await events.eventTeamsSimple(TBA_TOKEN, "2023ilch");
+
+    for (let i = 0; i < teams.length;i++) {
+        console.log(teams[i]);
+    }
+})();
+
